43734b31cc08590532d19775d3b9bd5cfaaa0c74
Dominic Neo <dom.neo.ws@gmail.com>
2022-06-04 19:02:52 +0800

    responsive footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,19 +4,6 @@ import styles from "../styles/Footer.module.scss";
 
 export const Footer = () => (
   <footer className={styles.nav}>
-    <div className={styles.footerMenuLeft}>
-      <nav className={styles.menu}>
-        <Link href="https://drive.google.com/file/d/1duXXtN5TqUb-7N13I96rup3lnkH0bhqk/view?usp=sharing">
-          <a className={styles.menuItem}>RESUME</a>
-        </Link>
-        <Link href="mailto:francinelim21@gmail.com">
-          <a className={styles.menuItem}>EMAIL</a>
-        </Link>
-        <Link href="https://www.linkedin.com/in/francine-lim/">
-          <a className={styles.menuItem}>LINKEDIN</a>
-        </Link>
-      </nav>
-    </div>
     <div className={styles.footerStars}>
       <div className={styles.stars}>
         <svg
@@ -49,15 +36,28 @@ export const Footer = () => (
         </svg>
       </div>
     </div>
+    <div className={styles.footerMenuLeft}>
+      <nav className={styles.menu}>
+        <Link href="https://drive.google.com/file/d/1duXXtN5TqUb-7N13I96rup3lnkH0bhqk/view?usp=sharing">
+          <a className={styles.menuItem}>RESUME</a>
+        </Link>
+        <Link href="mailto:francinelim21@gmail.com">
+          <a className={styles.menuItem}>EMAIL</a>
+        </Link>
+        <Link href="https://www.linkedin.com/in/francine-lim/">
+          <a className={styles.menuItem}>LINKEDIN</a>
+        </Link>
+      </nav>
+    </div>
     <div className={styles.footerMenuRight}>
       <nav className={styles.menu}>
-        <small>DESIGNED BY ME!</small>
+        <span className="caption">DESIGNED BY ME!</span>
         <Link href="https://www.domneo.dev">
           <a className={styles.menuItem} target="_blank">
             BUILT BY DOMN
           </a>
         </Link>
-        <small>©2022</small>
+        <span className="caption">©2022</span>
       </nav>
     </div>
   </footer>
