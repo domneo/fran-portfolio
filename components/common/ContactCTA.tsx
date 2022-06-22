e3947ffbce3919530f0fdd28df2a785d665bbdb4
Dominic Neo <dom.neo.ws@gmail.com>
2022-06-22 19:46:51 +0800

    Make blob stop on contact CTA hover

--- components/common/ContactCTA.tsx
+++ components/common/ContactCTA.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Blob from "components/common/Blob";
 
 interface ContactCTAProps {
@@ -5,24 +7,30 @@ interface ContactCTAProps {
   spacer?: "sm" | "md" | "lg";
 }
 
-export const ContactCTA = ({ children, spacer }: ContactCTAProps) => (
-  <div className={`row justify-content-center spacer-${spacer || ""}`}>
-    <div className="col-lg-8 text-center">
-      {children && (
-        <h2 className="display-5 text-bone spacer-md">{children}</h2>
-      )}
-      <div className="d-flex justify-content-center">
-        <a
-          href={`mailto:${process.env.NEXT_PUBLIC_EMAIL}`}
-          className="contact-cta d-block position-relative text-decoration-none"
-          style={{
-            width: "280px",
-          }}
-        >
-          <Blob />
-          <div className="contact-cta-text h5 fw-light">Contact</div>
-        </a>
+export const ContactCTA = ({ children, spacer }: ContactCTAProps) => {
+  const [isHovering, setIsHovering] = useState(false);
+
+  return (
+    <div className={`row justify-content-center spacer-${spacer || ""}`}>
+      <div className="col-lg-8 text-center">
+        {children && (
+          <h2 className="display-5 text-bone spacer-md">{children}</h2>
+        )}
+        <div className="d-flex justify-content-center">
+          <a
+            href={`mailto:${process.env.NEXT_PUBLIC_EMAIL}`}
+            className="contact-cta d-block position-relative text-decoration-none"
+            style={{
+              width: "280px",
+            }}
+            onMouseEnter={() => setIsHovering(true)}
+            onMouseLeave={() => setIsHovering(false)}
+          >
+            <Blob isHovering={isHovering} />
+            <div className="contact-cta-text h5 fw-light mb-2">Contact</div>
+          </a>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
