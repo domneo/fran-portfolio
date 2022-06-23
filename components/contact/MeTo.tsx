0a1c921cebe4f8e92518d2bd7676abb1e2d555fb
Dominic Neo <dom.neo.ws@gmail.com>
2022-06-23 21:18:51 +0800

    fix me to

---
+++ components/contact/MeTo.tsx
@@ -0,0 +1,45 @@
+import { Component } from "react";
+import Slider, { Settings } from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+
+interface MeToProps {
+  lines: Array<string>;
+}
+
+export class MeTo extends Component<MeToProps> {
+  constructor(props: MeToProps) {
+    super(props);
+  }
+
+  render() {
+    const settings: Settings = {
+      accessibility: false,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      draggable: false,
+      infinite: true,
+      pauseOnHover: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      touchMove: false,
+      vertical: true,
+    };
+
+    const { lines } = this.props;
+
+    return (
+      <>
+        <div className="h3 me-3 flex-shrink-0">
+          <div>me to</div>
+        </div>
+        <Slider {...settings}>
+          {lines.map((line) => (
+            <h3>{line}</h3>
+          ))}
+        </Slider>
+      </>
+    );
+  }
+}
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,52 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 
 import styles from "styles/Contact.module.scss";
 
 import Layout from "components/Layout";
 import { Arrows } from "components/contact/Arrows";
+import { MeTo } from "components/contact/MeTo";
 
 export default function Contact() {
-  const animDuration = 600;
-  const animDelay = 1000;
-  const slideCount = 6;
-
-  // Get height of ticker
-  const meToRef = useRef<HTMLDivElement>(null);
-  const [meToHeight, setMeToHeight] = useState(0);
-  useEffect(() => {
-    const height = meToRef.current?.offsetHeight || 61;
-    setMeToHeight(height);
-  }, []);
-
-  // Change current slide every x seconds
-  const [currentAnimDuration, setCurrentAnimDuration] = useState(animDuration);
-  const [meToIndex, setMeToIndex] = useState(0);
-  useEffect(() => {
-    // Reset animation duration if at the first slide
-    if (meToIndex === 0) setCurrentAnimDuration(animDuration);
-
-    const nextIndex = meToIndex === slideCount ? 0 : meToIndex + 1;
-    let timeout = setTimeout(
-      () => {
-        setMeToIndex(nextIndex);
-
-        // Jump to the first slide after animating to last slide
-        if (nextIndex === slideCount) {
-          setTimeout(() => {
-            setCurrentAnimDuration(0);
-            setMeToIndex(0);
-          }, animDuration);
-        }
-      },
-      // First slide delay should account for animation duration
-      meToIndex === 0 ? animDelay - animDuration : animDelay
-    );
-
-    return () => clearTimeout(timeout);
-  }, [meToIndex]);
-
   const yesdogRef = useRef<HTMLDivElement>(null);
   const linksRef = useRef<HTMLDivElement>(null);
 
@@ -63,28 +25,16 @@ export default function Contact() {
       <div className="container position-relative" style={{ zIndex: "1" }}>
         <div className="row">
           <div className="col offset-lg-2 overflow-hidden d-flex mt-5">
-            <div className="h3 me-3 flex-shrink-0">
-              <div ref={meToRef}>me to</div>
-            </div>
-            <div style={{ height: `${meToHeight}px` }}>
-              <div
-                style={{
-                  marginTop: `-${meToHeight * meToIndex}px`,
-                  transition: `${currentAnimDuration / 1000}s margin`,
-                }}
-              >
-                <h3>tell me bad jokes</h3>
-                <h3>collaborate on interesting projects</h3>
-                <h3>rage when pixels and guides aren&apos;t snapping</h3>
-                <h3>work on design systems</h3>
-                <h3>rave about games, ceramics, oddities, coffee and tea</h3>
-                <h3>
-                  dream up smooth experiences with functional and beautiful
-                  designs
-                </h3>
-                <h3>tell me bad jokes</h3>
-              </div>
-            </div>
+            <MeTo
+              lines={[
+                "tell me bad jokes",
+                "collaborate on interesting projects",
+                "rage when pixels and guides aren't snapping",
+                "work on design systems",
+                "rave about games, ceramics, oddities, coffee and tea",
+                "dream up smooth experiences with functional and beautiful designs",
+              ]}
+            />
           </div>
         </div>
         <div className="row">
