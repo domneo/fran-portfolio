3407f02f4b0dba76202b58a29fce0ce0e68b215f
Dominic Neo <dom.neo.ws@gmail.com>
2023-06-14 22:58:31 +0800

    global styles

--- pages/test.tsx
+++ pages/test.tsx
@@ -15,19 +15,21 @@ export default function Home() {
           <meta name="description" content="A UX Designer portfolio website" />
           <link rel="icon" href="/images/favicon.svg" />
         </Head>
-        <div style={{ width: "308px", height: "308px" }}>
-          <Blob />
-        </div>
-        <h1 className="display-1">Display 1</h1>
-        <h2 className="display-2">Display 2</h2>
-        <h3 className="display-3">Display 3</h3>
-        <h4 className="display-4">Display 4</h4>
-        <h5 className="display-5">Display 5</h5>
-        <h1>Heading 1</h1>
-        <h2>Heading 2</h2>
-        <h3>Heading 3</h3>
-        <h4>Heading 4</h4>
-        <h5>Heading 5</h5>
+
+        <h1 className="extralight">Heading 1 Extra Light</h1>
+        <h1>Heading 1 Regular</h1>
+        <h1 className="medium">Heading 1 Medium</h1>
+
+        <h2 className="extralight">Heading 2 Extra Light</h2>
+        <h2>Heading 2 Regular</h2>
+        <h2 className="medium">Heading 2 Medium</h2>
+
+        <h3 className="extralight">Heading 3 Extra Light</h3>
+        <h3 className="light">Heading 3 Light</h3>
+        <h3>Heading 3 Regular</h3>
+        <h3 className="medium">Heading 3 Medium</h3>
+        <h3 className="bold">Heading 3 Bold</h3>
+
         <p>
           This is first paragraph text. This is paragraph text. This is
           paragraph text. This is paragraph text. This is paragraph text.
@@ -36,10 +38,10 @@ export default function Home() {
           This is second paragraph text. This is paragraph text. This is
           paragraph text. This is paragraph text. This is paragraph text.
         </p>
+        <p className="caption">This is a line of caption text.</p>
         <p>
           <small>This is a line of small text.</small>
         </p>
-        <p className="caption">This is a line of caption text.</p>
         <Link href={"#"}>This is a text with link.</Link>
       </div>
     </Layout>
