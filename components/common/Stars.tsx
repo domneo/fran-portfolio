2089243f092b536222ef61f604cc262144fcfc63
Dominic Neo <dom.neo.ws@gmail.com>
2023-06-16 21:56:23 +0800

    animate stars while loading

--- components/common/Stars.tsx
+++ components/common/Stars.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import { useEffect, useMemo, useState } from "react";
 
 interface StarsProps {
@@ -43,6 +44,25 @@ export const Stars = ({ resetColor = "black", children }: StarsProps) => {
       setEColor(_resetColor);
     }
   }, [isAnimating, cycleIndex, colorCycleMatrix]);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = (url: string) =>
+      url !== router.asPath && setIsAnimating(true);
+    const handleComplete = (url: string) =>
+      url === router.asPath && setIsAnimating(false);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  });
 
   return (
     <div
