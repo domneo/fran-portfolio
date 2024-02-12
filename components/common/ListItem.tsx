420b7bfb0b4bb6941561faa1132dec04e0f77654
Dominic Neo <dom.neo.ws@gmail.com>
2024-02-12 17:24:00 +0800

    Remove unused files

--- components/common/ListItem.tsx
+++
@@ -1,9 +0,0 @@
-import removeWidows from "hooks/useRemoveWidows";
-
-interface ListItemProps {
-  children?: React.ReactNode;
-  className?: string;
-}
-export const ListItem = ({ children, className }: ListItemProps) => {
-  return <li className={className}>{removeWidows(children)}</li>;
-};
--- components/common/Paragraph.tsx
+++
@@ -1,9 +0,0 @@
-import removeWidows from "hooks/useRemoveWidows";
-
-interface ParagraphProps {
-  children?: React.ReactNode;
-  className?: string;
-}
-export const Paragraph = ({ children, className }: ParagraphProps) => {
-  return <p className={className}>{removeWidows(children)}</p>;
-};
