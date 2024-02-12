8a8866d433da99b7935ed2f9baf21b3ea7f79b6c
Dominic Neo <dom.neo.ws@gmail.com>
2024-02-12 19:06:48 +0800

    Contact page tweaks

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Layout from "components/Layout";
 import { BarrelLink } from "components/common/BarrelLink";
 import { MeToSlider } from "components/contact/MeToSlider";
@@ -27,47 +28,63 @@ export default function Contact({
   const { data: globalData } = useTina<GlobalQuery>(global);
   const contactLinks = globalData.global.contactLinks;
 
+  const [darkMode, setDarkMode] = useState(true);
+
   return (
     <Layout
       data={globalData}
-      darkMode
+      darkMode={darkMode}
       showQuickActions={false}
       showContactLinks={false}
     >
-      <div className={styles.titleWrapper}>
-        <div className={styles.title}>
-          <div>CO</div>
-          <div>CONTACT</div>
-          <div>CONTACT CONTACT</div>
-          <div>CONTACT CONTACT CONTACT</div>
-          <div>
-            CONTACT <span className="text-wenge">CONTACT</span>
-          </div>
-          <div>CONTACT</div>
-          <div>CO</div>
-        </div>
-      </div>
-      <div className={styles.body}>
-        <MeToSlider
-          lines={[
-            "me to tell me bad jokes",
-            "me to collaborate on interesting projects",
-            "me to rage when pixels and guides aren't snapping",
-            "me to work on design systems",
-            "me to rave about games, ceramics, oddities, coffee and tea",
-            "me to dream up smooth experiences with functional and beautiful designs",
-          ]}
-        />
-        <div className={styles.contactLinks}>
-          {contactLinks?.map((link) => (
-            <div key={window.crypto.randomUUID()} className={styles.menuItem}>
-              <BarrelLink
-                text={link?.name.toUpperCase() || ""}
-                link={link?.url || ""}
-                target={link?.openInNewWindow ? "_blank" : "_self"}
-              />
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.titleWrapper}>
+            <div className={styles.title}>
+              <div>CO</div>
+              <div>CONTACT</div>
+              <div>CONTACT CONTACT</div>
+              <div>CONTACT CONTACT CONTACT</div>
+              <div>
+                CONTACT{" "}
+                <button
+                  className="bg-transparent border-0 p-0 m-0 rounded-4 text-wenge"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => setDarkMode(!darkMode)}
+                >
+                  CONTACT
+                </button>
+              </div>
+              <div>CONTACT</div>
+              <div>CO</div>
             </div>
-          ))}
+          </div>
+          <div className={styles.body}>
+            <MeToSlider
+              lines={[
+                "me to tell me bad jokes",
+                "me to collaborate on interesting projects",
+                "me to rage when pixels and guides aren't snapping",
+                "me to work on design systems",
+                "me to rave about games, ceramics, oddities, coffee and tea",
+                "me to dream up smooth experiences with functional and beautiful designs",
+              ]}
+            />
+            <div className={styles.contactLinks}>
+              {contactLinks?.map((link) => (
+                <div
+                  key={window.crypto.randomUUID()}
+                  className={styles.menuItem}
+                >
+                  <BarrelLink
+                    text={link?.name.toUpperCase() || ""}
+                    link={link?.url || ""}
+                    target={link?.openInNewWindow ? "_blank" : "_self"}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
